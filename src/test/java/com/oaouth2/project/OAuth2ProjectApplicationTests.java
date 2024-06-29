fbb5f81291aaf037f7871cd7dc4d9c16f717f05f
RahulGupta <rahulrgd21@gmail.com>
2024-06-29 11:26:28 +0530

    Initial commit

---
+++ src/test/java/com/oaouth2/project/OAuth2ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oaouth2.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
